d90fba9c5f3a40418c119058cc7e6be6fb235c98
Djamel <djameldev@gmail.com>
2023-08-20 19:53:03 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Djamel Slimani",
+  role: "développeur front-end",
+  linkedinId: "https://www.linkedin.com/in/djamel-k-294476289/",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " J'aime le développement web, j'apprécie vraiment le fait de pouvoir découvrir de nouvelles technologies au quotidien. J'aime également beaucoup le football et jouer à des jeux pendant mon temps libre.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/djamel-k-294476289/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/DjamelDev/",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/rbhatia46",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "Développeur web junior, j'ai déjà parcouru un chemin professionnel riche et diversifié. Avant de me lancer dans le développement web, j'étais dans la vente. Ces années dans ce milieu m'ont permis de développer de solides compétences relationnelles. De plus, il faut savoir que depuis mon plus jeune âge j'aime énormément le football et j'en ai d'ailleurs pratiqué pendant 15 ans, j'ai donc appris l'importance du travail d'équipe, de la discipline et de l'engagement.",
+  address: "France",
+  website: "http://localhost:3000/",
+  education: [
+    {
+      UniversityName: "The LNM Insitute of Information Technology",
+      specialization: "Some specialization",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2020",
+      Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "Je ne perds jamais. Soit je gagne, soit j'apprends.",
+      name: "Nelson Mandela",
+    },
+    {
+      description:
+        "Dans 20 ans, tu seras plus déçu par les choses que tu n'as pas faites que par celles que tu auras faites. Alors, sors des sentiers battus. Mets les voiles. Explore. Rêve. Découvre.",
+      name: "Mark Twain",
+    },
+  ],
+};
+
+export default resumeData;
